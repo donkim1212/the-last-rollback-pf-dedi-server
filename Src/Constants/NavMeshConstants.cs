f84fbcf79a28d1ac89338e9ad752b5297bc22aca
Dongkyun Kim <donkim0122@gmail.com>
2024-08-16 00:50:53 +0900

    Add: NavMeshConstants, e.g., names of navmesh files

---
+++ Src/Constants/NavMeshConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace PathfindingDedicatedServer.Src.Constants
+{
+    public class NavMeshConstants
+    {
+        public const string DUNGEON_01_NAVMESH_FILENAME = "dungeon_01.navmesh";
+        public const string DUNGEON_01_OBJ_FILENAME = "dungeon_01.obj";
+    }
+}
