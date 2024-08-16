5e6615abeb35762d1c964a4b9ebc7ffe7b938697
Dongkyun Kim <donkim0122@gmail.com>
2024-08-16 15:09:53 +0900

    Add: separate RcConfig related lines from NavMeshLoader to a new file Add: NavMeshes class that stores navmeshes from assets Update: LoadAllNavMeshAssets method that finds and loads all .navmesh files within Assets directory then saves it in NavMeshes list Update: call LoadAllNavMeshAssets() instead of LoadNavMesh()

--- Program.cs
+++ Program.cs
@@ -11,109 +11,73 @@ using PathfindingDedicatedServer.Src.Nav;
 namespace PathfindingDedicatedServer;
 public class Program
 {
-    public static void Main()
+  public static void Main()
+  {
+    // Load all NavMeshes
+    NavMeshLoader.LoadAllNavMeshAssets();
+
+    //InputGeomProvider? geom = NavMeshLoader.LoadInputMesh(NavMeshConstants.DUNGEON_01_OBJ_FILENAME);
+    //if (geom != null)
+    //{
+    //    Console.WriteLine("verts:" + geom.GetMesh().GetVerts());
+    //}
+
+    // Start the TCP server
+    StartTcpServer();
+  }
+
+  private static void StartTcpServer()
+  {
+    // Set up the TCP listener on port 5000
+    IPAddress localhost = IPAddress.Parse("127.0.0.1");
+    TcpListener tcpListener = new(localhost, 5000);
+    tcpListener.Start();
+    Console.WriteLine("TCP Server started on port 5000.");
+
+    while (true)
     {
-        DtNavMesh? mesh = NavMeshLoader.LoadNavMesh(NavMeshConstants.DUNGEON_01_NAVMESH_FILENAME);
-        if (mesh != null)
+      try
+      {
+        // Accept a pending client connection
+        TcpClient tcpClient = tcpListener.AcceptTcpClient();
+        Console.WriteLine("Client connected.");
+
+        // Handle the client connection in a separate thread
+        Thread clientThread = new(start: HandleClient)
         {
-            Console.WriteLine($"{NavMeshConstants.DUNGEON_01_NAVMESH_FILENAME} loaded succesfully.");
-            Console.WriteLine("MaxTiles: " + mesh.GetMaxTiles());
-            Console.WriteLine("MaxVertsPerPoly: " + mesh.GetMaxVertsPerPoly());
-        }
+          IsBackground = true
+        };
+        clientThread.Start(tcpClient);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error: {ex.Message}");
+      }
+    }
+  }
 
-        //InputGeomProvider? geom = NavMeshLoader.LoadInputMesh(NavMeshConstants.DUNGEON_01_OBJ_FILENAME);
-        //if (geom != null)
-        //{
-        //    Console.WriteLine("verts:" + geom.GetMesh().GetVerts());
-        //}
+  private static async void HandleClient(object? obj)
+  {
+    try
+    {
+      if (obj is not TcpClient tcpClient)
+      {
+        return;
+      }
 
-        // Start the TCP server
-        StartTcpServer();
+      TcpClientHandler.TcpClientHandler handler = new (tcpClient);
+
+      handler.OnDataReceived += (data) =>
+      {
+        Console.WriteLine($"Received: {data}");
+      };
+
+      await handler.StartHandlingClientAsync();
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine("HandleClient Error:" + e.Message);
     }
 
-    private static void StartTcpServer()
-    {
-        // Set up the TCP listener on port 5000
-        IPAddress localhost = IPAddress.Parse("127.0.0.1");
-        TcpListener tcpListener = new(localhost, 5000);
-        tcpListener.Start();
-        Console.WriteLine("TCP Server started on port 5000.");
-
-        while (true)
-        {
-            try
-            {
-                // Accept a pending client connection
-                TcpClient tcpClient = tcpListener.AcceptTcpClient();
-                Console.WriteLine("Client connected.");
-
-                // Handle the client connection in a separate thread
-                Thread clientThread = new(start: HandleClient)
-                {
-                    IsBackground = true
-                };
-                clientThread.Start(tcpClient);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
-        }
-    }
-
-    private static async void HandleClient(object? obj)
-    {
-        try
-        {
-            if (obj is not TcpClient tcpClient)
-            {
-                return;
-            }
-
-            TcpClientHandler.TcpClientHandler handler = new (tcpClient);
-
-            handler.OnDataReceived += (data) =>
-            {
-                Console.WriteLine($"Received: {data}");
-            };
-
-            await handler.StartHandlingClientAsync();
-
-            /*NetworkStream stream = tcpClient.GetStream();
-            byte[] buffer = new byte[1024];
-
-            while (tcpClient.Connected)
-            {
-                try
-                {
-                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                    if (bytesRead == 0)
-                    {
-                        Console.WriteLine("??");
-                        break;
-                    }
-
-                    string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine($"Received: {request}");
-
-                    // Echo the message back to the client
-                    byte[] response = Encoding.UTF8.GetBytes("Hello from TCP server!");
-                    stream.Write(response, 0, response.Length);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"HandleClient Error: {e.Message}");
-                    break;
-                }
-            }
-
-            // Close the client connection
-            tcpClient.Close();*/
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("HandleClient Error:" + e.Message);
-        }
-
-    }
+  }
 }
--- Src/Nav/NavMeshLoader.cs
+++ Src/Nav/NavMeshLoader.cs
@@ -1,135 +1,134 @@
 ﻿using System;
+using System.IO;
 using DotRecast.Core;
 using DotRecast.Detour;
 using DotRecast.Detour.Io;
 using DotRecast.Recast;
+using PathfindingDedicatedServer.Src.Constants;
+using PathfindingDedicatedServer.Src.Nav.Config;
 
 namespace PathfindingDedicatedServer.Src.Nav;
 public class NavMeshLoader
 {
-    private const RcPartition RC_PARTITION_TYPE = RcPartition.WATERSHED;
-    private const float CELL_SIZE = 0.3f;
-    private const float CELL_HEIGHT = 0.2f;
-    private const float AGENT_MAX_SLOPE = 20f;
-    private const float AGENT_HEIGHT = 1.0f;
-    private const float AGENT_RADIUS = 0.0f;
-    private const float AGENT_MAX_CLIMB = 2.0f;
-    private const int REGION_MIN_SIZE = 60;
-    private const int REGION_MERGE_SIZE = 20;
-    private const float EDGE_MAX_LENGTH = 12.5f;
-    private const float EDGE_MAX_ERROR = 1.6f;
-    private const int VERTS_PER_POLY = 6;
-    private const float DETAIL_SAMPLE_DIST = 6.0f;
-    private const float DETAIL_SAMPLE_MAX_ERROR = 2.0f;
-    private const bool FILTER_LOW_HANGING_OBSTACLES = true;
-    private const bool FILTER_LEDGE_SPANS = true;
-    private const bool FILTER_WALKABLE_LOW_HEIGHT_SPANS = true;
-    private const int AREA_ID = RcRecast.RC_WALKABLE_AREA;
-    private static readonly RcAreaModification RC_AREA_MODIFICATION = new(AREA_ID);
-    private const bool BUILD_MESH_DETAIL = false;
-
-    private const string ASSETS_REL_PATH_PREFIX = "./Assets/";
-
-    /// <summary>
-    /// Loads NavMesh from Assets directory.
-    /// </summary>
-    /// <param name="filename">name of the navmesh file with extension included</param>
-    /// <returns>DtNavMesh, or null if loading failed</returns>
-    public static DtNavMesh? LoadNavMesh(string filename)
+  /// <summary>
+  /// Reads all files in Assets directory and loads all .NAVMESH files into NavMeshes list.
+  /// </summary>
+  public static void LoadAllNavMeshAssets()
+  {
+    try
     {
-        try
+      // TODO: map the indices somehow
+      int index = 1;
+      string[] files = Directory.GetFiles(PathConstants.ASSETS_REL_PATH, PathConstants.NAVMESH_EXT, SearchOption.AllDirectories);
+      foreach (string file in files)
+      {
+        DtNavMesh? navMesh = LoadNavMesh(file);
+        if (navMesh != null)
         {
-            ArgumentNullException.ThrowIfNull(filename);
-            FileStream fs = File.OpenRead(ASSETS_REL_PATH_PREFIX + filename);
-            BinaryReader br = new(fs);
-
-            DtMeshSetReader reader = new();
-
-            DtNavMesh navMesh = reader.Read(br, VERTS_PER_POLY);
-            if (navMesh == null)
-            {
-                throw new InvalidOperationException("LoadNavMesh failed to read NavMesh.");
-            }
-
-            return navMesh;
+          NavMeshes.AddNavMesh(index, navMesh);
+          Console.WriteLine($"Loaded file: {file}");
+          float[] verts = navMesh.GetTile(1).data.verts;
+          // Testing
+          Console.WriteLine($"-- id: {index} | Tile[1] verts: {verts[0]} {verts[1]} {verts[2]}");
         }
-        catch (Exception e)
+        else
         {
-            Console.WriteLine("LoadNavMesh Error: " + e.Message);
-            return null;
+          Console.WriteLine($"Failed loading file: {file}");
         }
+        index++;
+      }
+    } catch (Exception e)
+    {
+      Console.WriteLine("LoadAllNavMeshAssets Error: " + e.Message);
+    }
+  }
+
+  /// <summary>
+  /// Loads NavMesh from Assets directory.
+  /// </summary>
+  /// <param name="path">name of the navmesh file with extension included</param>
+  /// <returns>DtNavMesh, or null if loading failed</returns>
+  public static DtNavMesh? LoadNavMesh(string path)
+  {
+    try
+    {
+      ArgumentNullException.ThrowIfNull(path);
+      FileStream fs = File.OpenRead(path);
+      BinaryReader br = new(fs);
+
+      DtMeshSetReader reader = new();
+
+      DtNavMesh navMesh = reader.Read(br, MyRcConfig.VERTS_PER_POLY);
+      if (navMesh == null)
+      {
+        throw new InvalidOperationException("LoadNavMesh failed to read NavMesh.");
+      }
+
+      return navMesh;
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine("LoadNavMesh Error: " + e.Message);
+      return null;
+    }
+  }
+
+  public static InputGeomProvider? LoadInputMesh(string path)
+  {
+    try
+    {
+      RcObjImporterContext ctx = LoadObjFromFile(path);
+      // List<float> vertexPositions
+      // List<int> meshFaces
+      InputGeomProvider geom = new(ctx);
+      return geom;
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine("LoadInputMesh Error: " + e.Message);
+      return null;
+    }
+  }
+
+  private static RcObjImporterContext LoadObjFromFile(string path)
+  {
+    ArgumentNullException.ThrowIfNull(path);
+    byte[] navData = File.ReadAllBytes(path);
+    return RcObjImporter.LoadContext(navData);
+  }
+
+  /// <summary>
+  /// Incomplete
+  /// </summary>
+  /// <param name="ctx"></param>
+  /// <returns></returns>
+  private static RcPolyMesh ConvertObjToPolyMesh(RcObjImporterContext ctx)
+  {
+    RcPolyMesh rcPolyMesh = new()
+    {
+      nverts = ctx.vertexPositions.Count / 3,
+      verts = new int[ctx.vertexPositions.Count],
+      nvp = 3,
+      polys = new int[ctx.meshFaces.Count],
+      maxpolys = ctx.meshFaces.Count,
+      regs = new int[ctx.meshFaces.Count],
+      areas = new int[ctx.meshFaces.Count],
+      flags = new int[ctx.meshFaces.Count],
+      bmin = new DotRecast.Core.Numerics.RcVec3f(),
+      bmax = new DotRecast.Core.Numerics.RcVec3f(),
+      cs = 0.3f,
+      ch = 0.2f
+    };
+
+    for (int i = 0; i < rcPolyMesh.nverts; i++)
+    {
+      //rcPolyMesh.verts[i * 3] = (int)()
     }
 
-    public static InputGeomProvider? LoadInputMesh(string filename)
-    {
-        try
-        {
-            RcObjImporterContext ctx = LoadObjFromFile(ASSETS_REL_PATH_PREFIX + filename);
-            // List<float> vertexPositions
-            // List<int> meshFaces
-            InputGeomProvider geom = new(ctx);
-            return geom;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("LoadInputMesh Error: " + e.Message);
-            return null;
-        }
+    // temp
+    return new RcPolyMesh();
+  }
 
-        //RcConfig config = new(
-        //    RC_PARTITION_TYPE,
-        //    CELL_SIZE, CELL_HEIGHT,
-        //    AGENT_MAX_SLOPE, AGENT_HEIGHT, AGENT_RADIUS, AGENT_MAX_CLIMB,
-        //    REGION_MIN_SIZE, REGION_MERGE_SIZE,
-        //    EDGE_MAX_LENGTH, EDGE_MAX_ERROR,
-        //    VERTS_PER_POLY,
-        //    DETAIL_SAMPLE_DIST, DETAIL_SAMPLE_MAX_ERROR,
-        //    FILTER_LOW_HANGING_OBSTACLES, FILTER_LEDGE_SPANS, FILTER_WALKABLE_LOW_HEIGHT_SPANS,
-        //    RC_AREA_MODIFICATION, BUILD_MESH_DETAIL
-        //);
-
-        //RcMeshs.BuildPolyMesh(new RcContext(), );
-    }
-
-    private static RcObjImporterContext LoadObjFromFile(string path)
-    {
-        ArgumentNullException.ThrowIfNull(path);
-        byte[] navData = File.ReadAllBytes(path);
-        return RcObjImporter.LoadContext(navData);
-    }
-
-    /// <summary>
-    /// Incomplete
-    /// </summary>
-    /// <param name="ctx"></param>
-    /// <returns></returns>
-    private static RcPolyMesh ConvertObjToPolyMesh(RcObjImporterContext ctx)
-    {
-        RcPolyMesh rcPolyMesh = new()
-        {
-            nverts = ctx.vertexPositions.Count / 3,
-            verts = new int[ctx.vertexPositions.Count],
-            nvp = 3,
-            polys = new int[ctx.meshFaces.Count],
-            maxpolys = ctx.meshFaces.Count,
-            regs = new int[ctx.meshFaces.Count],
-            areas = new int[ctx.meshFaces.Count],
-            flags = new int[ctx.meshFaces.Count],
-            bmin = new DotRecast.Core.Numerics.RcVec3f(),
-            bmax = new DotRecast.Core.Numerics.RcVec3f(),
-            cs = 0.3f,
-            ch = 0.2f
-        };
-
-        for (int i = 0; i < rcPolyMesh.nverts; i++)
-        {
-            //rcPolyMesh.verts[i * 3] = (int)()
-        }
-
-        // temp
-        return new RcPolyMesh();
-    }
-
-    //DtNavMesh
-    //DtNavMeshBuilder
+  //DtNavMesh
+  //DtNavMeshBuilder
 }
