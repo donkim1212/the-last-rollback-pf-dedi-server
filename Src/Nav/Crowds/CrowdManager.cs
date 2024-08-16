559de5e88c614b37a58da53e14229a45d9606369
Dongkyun Kim <donkim0122@gmail.com>
2024-08-16 21:06:00 +0900

    Feat: game loop, adding/removing monsters, etc Update: remove unnecessary fields

--- Src/Nav/Crowds/CrowdManager.cs
+++ Src/Nav/Crowds/CrowdManager.cs
@@ -1,29 +1,118 @@
-﻿using DotRecast.Detour;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DotRecast.Core.Numerics;
+using DotRecast.Detour;
 using DotRecast.Detour.Crowd;
+using DotRecast.Recast;
+using PathfindingDedicatedServer.Src.Constants;
 
 namespace PathfindingDedicatedServer.Src.Nav.Crowds
 {
+  public enum CrowdManagerState
+  {
+    NONE, WAITING, RUNNING, ENDING
+  }
+
   public class CrowdManager
   {
-    private DtCrowd _crowd;
-    private DtCrowdConfig _config;
-    private DtNavMesh _mesh;
+    private readonly DtCrowd _crowd;
+    private CrowdManagerState _state = CrowdManagerState.NONE;
 
-    public CrowdManager(int id) : this(new DtCrowdConfig(0.6f), NavMeshes.GetNavMesh(id))
+    private Dictionary<int, int> _monsters = [];
+    private DateTime _startTime;
+
+    //private Thread _thread;
+    private int _tickRate = 20; // 0.02s (FixedUpdate, will need rate limitting)
+
+    public CrowdManager(int dungeonCode) : this(new DtCrowdConfig(0.6f), NavMeshes.GetNavMesh(dungeonCode))
     {
     }
 
     public CrowdManager(DtCrowdConfig config, DtNavMesh? navMesh)
     {
-      if (navMesh == null)
-      {
-          throw new NullReferenceException($"CrowdManager: NavMesh is null");
-      }
-      _config = config;
-      _mesh = navMesh;
-      _crowd = new (config, navMesh);
+      ArgumentNullException.ThrowIfNull(navMesh);
+      _crowd = new(config, navMesh);
     }
 
+    public void Start()
+    {
+      // TODO: gameloop?
+      _state = CrowdManagerState.RUNNING;
+      Task.Run(() => GameLoop());
+    }
 
+    public void End()
+    {
+      // TODO: cleanup?
+      _state = CrowdManagerState.ENDING;
+    }
+
+    private async Task GameLoop()
+    {
+      _startTime = DateTime.Now;
+      DateTime prevTime = _startTime;
+      while (_state == CrowdManagerState.RUNNING)
+      {
+        DateTime curTime = DateTime.Now;
+        
+        float deltaTime = (float)(curTime - prevTime).TotalSeconds;
+        Update(deltaTime);
+      }
+    }
+
+    private void Update(float deltaTime)
+    {
+      _crowd.Update(deltaTime, null);
+    }
+
+    public void AddMonster(int monsterIdx, RcVec3f pos, DtCrowdAgentParams option)
+    {
+      //new DtCrowdAgentParams();
+      DtCrowdAgent agent = _crowd.AddAgent(pos, option);
+      _monsters.Add(monsterIdx, agent.idx);
+    }
+
+    public void AddMonster(int monsterIdx, DtCrowdAgentParams option)
+    {
+      AddMonster(monsterIdx, SpawnerConstants.GetRandomSpawnPosition(), option);
+    }
+
+    public void AddMonster(int monsterIdx)
+    {
+      // TODO: fetch monster's agent param stats from JSON
+      DtCrowdAgentParams option = new() // temp stats
+      {
+        radius = 0.6f,
+        height = 1.5f,
+        maxAcceleration = 5f,
+        maxSpeed = 3.5f,
+        
+      };
+      AddMonster(monsterIdx, option);
+    }
+
+    public DtCrowdAgent GetMonsterAgent (int monsterIdx)
+    {
+      return _crowd.GetAgent(_monsters[monsterIdx]);
+    }
+
+    public void RemoveMonster(int monsterIdx)
+    {
+      _crowd.RemoveAgent(GetMonsterAgent(monsterIdx));
+      _monsters.Remove(monsterIdx);
+    }
+
+    public void ClearMonsters()
+    {
+      foreach(int monsterIdx in _monsters.Keys)
+      {
+        RemoveMonster(monsterIdx);
+      }
+    }
+
+    public CrowdManagerState GetState()
+    {
+      return _state;
+    }
   }
 }
