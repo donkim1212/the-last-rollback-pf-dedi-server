047e3336598e8ceab2dd1e9c615a8c5ac8efccd5
Dongkyun Kim <donkim0122@gmail.com>
2024-08-16 15:10:53 +0900

    Add: Path related constants

---
+++ Src/Constants/PathConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace PathfindingDedicatedServer.Src.Constants
+{
+  public class PathConstants
+  {
+    public const string ASSETS_REL_PATH = "./Assets/";
+    public const string NAVMESH_EXT = "*.NAVMESH";
+    public const string WAVEFRONT_OBJ_EXT = "*.obj";
+  }
+}
