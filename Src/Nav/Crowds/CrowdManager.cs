7309926df61b4790ea516b6da1aef585fc6ca49e
Dongkyun Kim <donkim0122@gmail.com>
2024-08-16 15:11:55 +0900

    Add: CrowdManager draft

---
+++ Src/Nav/Crowds/CrowdManager.cs
@@ -0,0 +1,29 @@
+﻿using DotRecast.Detour;
+using DotRecast.Detour.Crowd;
+
+namespace PathfindingDedicatedServer.Src.Nav.Crowds
+{
+  public class CrowdManager
+  {
+    private DtCrowd _crowd;
+    private DtCrowdConfig _config;
+    private DtNavMesh _mesh;
+
+    public CrowdManager(int id) : this(new DtCrowdConfig(0.6f), NavMeshes.GetNavMesh(id))
+    {
+    }
+
+    public CrowdManager(DtCrowdConfig config, DtNavMesh? navMesh)
+    {
+      if (navMesh == null)
+      {
+          throw new NullReferenceException($"CrowdManager: NavMesh is null");
+      }
+      _config = config;
+      _mesh = navMesh;
+      _crowd = new (config, navMesh);
+    }
+
+
+  }
+}
