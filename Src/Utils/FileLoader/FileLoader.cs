1d9d7c6c41151622143ad34d95f1579190d61cf5
Dongkyun Kim <donkim0122@gmail.com>
2024-08-17 23:14:26 +0900

    Add: FileLoader (WIP)

---
+++ Src/Utils/FileLoader/FileLoader.cs
@@ -0,0 +1,14 @@
+﻿namespace PathfindingDedicatedServer.Src.Utils.FileLoader
+{
+  public abstract class FileLoader
+  {
+    public string[] LoadAllFilePaths (string dirPath, string fileExt)
+    {
+      string[] filePaths;
+      filePaths = Directory.GetFiles(dirPath, fileExt);
+      return filePaths;
+    }
+
+    public abstract void ReadFile (string filePath);
+  }
+}
