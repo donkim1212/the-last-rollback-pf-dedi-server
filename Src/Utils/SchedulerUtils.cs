f1a45a1bd0e414b21db66e16a6f44a46e63d9227
Dongkyun Kim <donkim0122@gmail.com>
2024-08-17 18:18:15 +0900

    Add: SetTimedAction that works like JS's setTimeout

---
+++ Src/Utils/SchedulerUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace PathfindingDedicatedServer.Src.Utils
+{
+  public class SchedulerUtils
+  {
+    /// <summary>
+    /// Sets a non-blocking async timed action.
+    /// </summary>
+    /// <param name="ms">waiting time in millis</param>
+    /// <param name="callback">callback function to run</param>
+    public static void SetTimedAction (int ms, Action callback)
+    {
+      _ = Task.Run(async () =>
+      {
+        await Task.Delay(ms);
+        callback();
+      });
+    }
+  }
+}
