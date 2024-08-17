16ef88aec6d0b14bae5b5824d4c11dd77b535f57
Dongkyun Kim <donkim0122@gmail.com>
2024-08-17 23:03:26 +0900

    Feat: add interval functions which work like JS's setInterval

--- Src/Utils/SchedulerUtils.cs
+++ Src/Utils/SchedulerUtils.cs
@@ -7,12 +7,33 @@
     /// </summary>
     /// <param name="ms">waiting time in millis</param>
     /// <param name="callback">callback function to run</param>
-    public static void SetTimedAction (int ms, Action callback)
+    public static void SetTimedAction(int ms, Action callback)
     {
       _ = Task.Run(async () =>
       {
         await Task.Delay(ms);
         callback();
+      });
+    }
+
+    public static void SetIntervalAction(int ms, Action callback)
+    {
+      _ = Task.Run(async () =>
+      {
+        await Task.Delay(ms);
+        callback();
+        SetIntervalAction(ms, callback);
+      });
+    }
+    
+    public static void SetIntervalAction(int ms, int count, Action callback)
+    {
+      if (count == 0) return;
+      _ = Task.Run(async () =>
+      {
+        await Task.Delay(ms);
+        callback();
+        SetIntervalAction(ms, count - 1, callback);
       });
     }
   }
