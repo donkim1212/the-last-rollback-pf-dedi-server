1d44191284dd61aa3f4a65d39c92fd67cf2fcc49
Dongkyun Kim <donkim0122@gmail.com>
2024-08-19 23:00:17 +0900

    Rename: Constants.cs -> ServerConstants.cs Update(NavMeshConstants.cs): use newer version of dungeon01 navmesh Del: SpawnerConstants (unused) Del: SpawnerManager (unused)

--- Src/Constants/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PathfindingDedicatedServer.Src.Constants
-{
-  internal class Constants
-  {
-    public const string VERSION = "1.0.0";
-  }
-}
---
+++ Src/Constants/ServerConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace PathfindingDedicatedServer.Src.Constants
+{
+  internal class ServerConstants
+  {
+    public static readonly string VERSION = "1.0.0";
+    public static readonly int MAX_CONNECTIONS = 10; // needs testing
+    public static readonly int RANDOM_SEED = 124136;
+  }
+}
