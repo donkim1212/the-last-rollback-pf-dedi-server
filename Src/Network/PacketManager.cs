5d7220056e5656137c935e96fdc021315fafcf74
Dongkyun Kim <donkim0122@gmail.com>
2024-08-19 09:32:59 +0900

    Update: add more handler mappings

--- Src/Network/PacketManager.cs
+++ Src/Network/PacketManager.cs
@@ -20,8 +20,8 @@ public class PacketManager
     _handlers.Add((int)Packet.PacketType.C_CreateSession, new CreateSessionHandler());
     _handlers.Add((int)Packet.PacketType.C_SetPlayers, new SetPlayersHandler());
     _handlers.Add((int)Packet.PacketType.C_SetMonsters, new SetMonstersHandler());
-    //_handlers.Add((int)Packet.PacketType.C_SetPlayerDest, new SetPlayerDestHandler());
-    //_handlers.Add((int)Packet.PacketType.C_SetMonsterDest, new SetMonsterDestHandler());
+    _handlers.Add((int)Packet.PacketType.C_SetPlayerDest, new SetPlayerDestHandler());
+    _handlers.Add((int)Packet.PacketType.C_SetMonsterDest, new SetMonsterDestHandler());
   }
 
   public Action<NetworkStream, Guid, byte[]> GetPacketHandler(int id)
