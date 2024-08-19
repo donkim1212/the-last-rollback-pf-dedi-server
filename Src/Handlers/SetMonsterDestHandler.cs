51c0eb6b61e47cda39c0a52afdeeb7dc1c054600
Dongkyun Kim <donkim0122@gmail.com>
2024-08-19 09:20:58 +0900

    Add: SetMonsterDest/SetPlayerDest Handlers

---
+++ Src/Handlers/SetMonsterDestHandler.cs
@@ -0,0 +1,18 @@
+﻿using PathfindingDedicatedServer.handlers.abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathfindingDedicatedServer.Src.Handlers
+{
+  internal class SetMonsterDestHandler : PacketHandler
+  {
+    public override void HandlePacket(NetworkStream stream, Guid id, byte[] bytes)
+    {
+      
+    }
+  }
+}
---
+++ Src/Handlers/SetPlayerDestHandler.cs
@@ -0,0 +1,13 @@
+﻿using PathfindingDedicatedServer.handlers.abstracts;
+using System.Net.Sockets;
+
+namespace PathfindingDedicatedServer.Src.Handlers
+{
+  internal class SetPlayerDestHandler : PacketHandler
+  {
+    public override void HandlePacket(NetworkStream stream, Guid id, byte[] bytes)
+    {
+      
+    }
+  }
+}
