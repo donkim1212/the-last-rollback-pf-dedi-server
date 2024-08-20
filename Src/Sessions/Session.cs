8b90354fcdcfa735368af337ce5c8f60566ffa7b
Dongkyun Kim <donkim0122@gmail.com>
2024-08-21 01:56:18 +0900

    Feat: add async Task GameLoop, save TcpClientHandler's Guid id as field variable

--- Src/Sessions/Session.cs
+++ Src/Sessions/Session.cs
@@ -1,5 +1,7 @@
 ﻿using PathfindingDedicatedServer.Nav.Crowds;
+using PathfindingDedicatedServer.Src.Network;
 using System.Net.Sockets;
+using static Packet;
 
 namespace PathfindingDedicatedServer.Src.Sessions
 {
@@ -8,10 +10,12 @@ namespace PathfindingDedicatedServer.Src.Sessions
     private static readonly Dictionary<Guid, Session> _sessions = [];
     
     private readonly NavManager _navManager;
+    private readonly Func<Task> _gameLoop;
+    private readonly Guid _id;
 
     public static bool AddSession(uint dungeonCode, Guid guid)
     {
-      return _sessions.TryAdd(guid, new Session(dungeonCode));
+      return _sessions.TryAdd(guid, new Session(dungeonCode, guid));
     }
 
     public static Session GetSession(Guid guid)
@@ -30,9 +34,72 @@ namespace PathfindingDedicatedServer.Src.Sessions
       return exists;
     }
     
-    public Session (uint dungeonCode)
+    public Session (uint dungeonCode, Guid id)
     {
       _navManager = new NavManager(dungeonCode);
+      _gameLoop = StartGameLoop;
+      _id = id;
+    }
+
+    public void Start()
+    {
+      _ = Task.Run(_gameLoop);
+    }
+
+    public async Task StartGameLoop ()
+    {
+      try
+      {
+        TcpClientHandler? clientHandler = TcpClientHandler.GetTcpClientHandler(_id);
+        if (clientHandler == null)
+        {
+          throw new InvalidOperationException("No TcpClientHandler found for the session.");
+        }
+        _navManager.Start();
+        while (_navManager.GetState() == NavManagerState.RUNNING)
+        {
+          var deltaTime = _navManager.UpdateImmediately();
+          Console.WriteLine($"deltaTime: {deltaTime}s");
+          clientHandler.SendPacket<S_MonstersLocationUpdate>(
+            PacketType.S_MonstersLocationUpdate,
+            _navManager.GetMonsterLocations()
+          );
+          clientHandler.SendPacket<S_PlayersLocationUpdate>(
+            PacketType.S_PlayerLocationUpdate,
+            _navManager.GetPlayerLocations()
+          );
+          
+          await Task.Delay((int)_navManager.GetMilliSecondsDelay());
+
+          // TODOs
+
+          // 1.
+          // increase aggro weight per tick?
+          // choose target with most aggro point
+          // base gets fixed weight value
+
+          // 2.
+          // calc distance btw each monster and its target
+          //  Vector2 distance - (monster r + target r) ??
+          // if the calc dist <= atk range, then
+          //  monster stops moving, emits atk packet
+          //  - option 1: waits until atk end packet arrives and resume tracking
+          //  - option 2: waits async for atk cooldown and retrack the prev target
+
+          // 3.
+        }
+        Console.WriteLine("Game loop ended.");
+      } catch (Exception e)
+      {
+        // TODO: print error msg & stack trace
+        Console.WriteLine(e.ToString());
+      } finally
+      {
+        // TODO: release resources
+        _navManager.End();
+        RemoveSession(_id); // both gets GC'd (hopefully)
+      }
+      
     }
 
     public NavManager GetNavManager()
