8b453b2e700d552791998f462fa3082d2ac5f075
Dongkyun Kim <donkim0122@gmail.com>
2024-08-22 00:59:07 +0900

    Feat: Vector3 calcuation util functions

--- Src/Utils/Utils.cs
+++ Src/Utils/Utils.cs
@@ -1,4 +1,5 @@
-﻿using DotRecast.Core.Numerics;
+﻿using DotRecast.Core;
+using DotRecast.Core.Numerics;
 
 namespace PathfindingDedicatedServer.Src.Utils
 {
@@ -22,6 +23,30 @@ namespace PathfindingDedicatedServer.Src.Utils
         Y = pos.Y,
         Z = pos.Z
       };
+    }
+
+    public static float CalcDistance (RcVec3f a, RcVec3f b)
+    {
+      RcVec3f diff = a - b;
+      return (float) Math.Sqrt(RcMath.Sqr(diff.X) + RcMath.Sqr(diff.Y) + RcMath.Sqr(diff.Z));
+    }
+
+    public static float CalcActualDistance (RcVec3f a, float aRad, RcVec3f b, float bRad)
+    {
+      return Math.Abs(CalcDistance(a, b) - (aRad + bRad));
+    }
+
+    public static RcVec3f CalcDirectionNormalized (RcVec3f from, RcVec3f to)
+    {
+      RcVec3f diff = from - to;
+      float magnitude = (float)Math.Sqrt(RcMath.Sqr(diff.X) + RcMath.Sqr(diff.Y) + RcMath.Sqr(diff.Z));
+      if (magnitude == 0)
+      {
+        // The two vectors are on the same position
+        return RcVec3f.Zero;
+      }
+
+      return diff * (1 / magnitude);
     }
   }
 }
