292c9ed8cb240c7693ef73e5427e56b4a53385f1
Dongkyun Kim <donkim0122@gmail.com>
2024-08-22 19:26:25 +0900

    Update: add tick rate constant, change some readonly statics to const

--- Src/Constants/ServerConstants.cs
+++ Src/Constants/ServerConstants.cs
@@ -2,8 +2,12 @@
 {
   internal class ServerConstants
   {
-    public static readonly string VERSION = "1.0.0";
-    public static readonly int MAX_CONNECTIONS = 10; // needs testing
-    public static readonly int RANDOM_SEED = 124136;
+    /* using */
+    public static readonly int RANDOM_SEED = DateTime.UtcNow.Millisecond;
+    public const int TICK_RATE = 50; // ms
+
+    /* unused */
+    public const string VERSION = "1.0.0";
+    public const int MAX_CONNECTIONS = 10; // needs testing
   }
 }
