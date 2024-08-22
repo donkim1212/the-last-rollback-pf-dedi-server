ce892d198ff9189f4f5bed3e46ba5c042430a3fe
Dongkyun Kim <donkim0122@gmail.com>
2024-08-22 22:13:46 +0900

    Fix: navmesh ext to lowercase due to case sensitivity in EC2

--- Src/Constants/PathConstants.cs
+++ Src/Constants/PathConstants.cs
@@ -3,7 +3,7 @@
   public class PathConstants
   {
     public const string ASSETS_REL_PATH = "./Assets/";
-    public const string NAVMESH_EXT = "*.NAVMESH";
+    public const string NAVMESH_EXT = "*.navmesh";
     public const string WAVEFRONT_OBJ_EXT = "*.obj";
     public const string JSON_EXT = "*.json";
   }
